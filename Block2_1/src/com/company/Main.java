d5d1dd26654196e0802b4d753e0c3f9f520f64f8
Кирилл <62066469+gumobu@users.noreply.github.com>
2021-02-04 17:03:54 +0300

    Add files via upload

--- Block2_1/src/com/company/Main.java
+++ Block2_1/src/com/company/Main.java
@@ -12,6 +12,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
 //        System.out.println("Результат 1.1: " + hiddenAnagram1(in));
 //        System.out.println("Результат 1.2: " + collect2(in));
+        System.out.println("Результат 1.3: " + nicoCipher3(in));
 //        System.out.println("Результат 1.4: " + twoProduct4(in));
 //        System.out.println("Результат 1.5: " + isExact5(in));
     }
@@ -106,6 +107,94 @@ public class Main {
         }
     }
 
+    public static String nicoCipher3(Scanner scanner) {
+        System.out.println("Введите строку: ");
+        String str = scanner.nextLine();
+        System.out.println("Введите ключ: ");
+        String key = scanner.nextLine();
+        int c = 0;
+        int strk = str.length() / key.length() + 2;
+        int stol = key.length();
+        char[] keynum = new char[key.length()];
+        char[][] darr = new char[strk][stol];
+        char[][] farr = new char[strk][stol];
+        for (int i = 0; i < key.length(); i++) {
+            int asc = (char) key.charAt(i);
+            for (int j = 0; j < key.length(); j++) {
+                int dopasc = (char) key.charAt(j);
+                if (dopasc <= asc) {
+                    c++;
+                }
+            }
+            keynum[i] = (char) (c + 48);
+            c = 0;
+        }
+        for (int i = 0; i < key.length(); i++) {
+            darr[0][i] = keynum[i];
+        }
+        int st = 1;
+        int k = 0;
+        for (int i = 0; i < stol; i++) {
+            if (k < str.length()) {
+                darr[st][i] = str.charAt(k);
+                k++;
+            }
+            else {
+                while(i < stol)
+                {
+                    darr[st][i] = ' ';
+                    i++;
+                }
+                break;
+            }
+            if (i + 1 == stol) {
+                st++;
+                i = -1;
+            }
+        }
+        st = 0;
+        for (int i = 0; i < stol; i++) {
+            if (i + 1 == stol) {
+                st++;
+                if (st > strk - 1) {
+                    break;
+                }
+                i = -1;
+            }
+        }
+
+        int n = 1;
+        int r = 0;
+        for (int i = 0; i < stol; i++) {
+            int asc = (char) darr[0][i] - 48;
+            if (asc == n) {
+                for (int j = 0; j < strk; j++) {
+                    farr[j][r] = darr[j][i];
+                }
+                r++;
+                i = -1;
+                n++;
+            }
+        }
+
+        st = 0;
+        for (int i = 0; i < stol; i++) {
+            if (i + 1 == stol) {
+                st++;
+                if (st > strk - 1) {
+                    break;
+                }
+                i = -1;
+            }
+        }
+        StringBuilder res = new StringBuilder();
+        for (int i = 1; i < strk; i++) {
+            for (int j = 0; j < stol; j++) {
+                res.append(farr[i][j]);
+            }
+        }
+        return res.toString();
+    }
 
     public static String twoProduct4(Scanner scanner){
         System.out.println("Введите количество элементов массива");
